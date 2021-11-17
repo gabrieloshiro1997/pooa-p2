20197d21d70817b735ba73ac523f0f07b1640372
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-11-17 20:15:57 -0300

    feat: add create recommendation

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
 
+const Recomendacao = require('./models/recomendacao')
+
 app.use(express.json())
 app.use(cors())
 
@@ -12,5 +14,16 @@ const { API_PORT, DATABASE_URL, DATABASE_PORT, DATABASE_NAME } = process.env
 mongoose.connect(`${DATABASE_URL}:${DATABASE_PORT}/${DATABASE_NAME}`)
   .then(() => console.log("OK"))
   .catch((error) => console.log("Erro: " + error))
+
+app.post('/api/recomendacao', (req, res) => {
+
+  const recomendacao = new Recomendacao({
+    descricao: req.body.descricao
+  })
+  recomendacao.save()
+    .then((recomendacaoInserida) => {
+      res.status(201).json(recomendacaoInserida)
+    })
+})
 
 app.listen(API_PORT, () => console.log(`App running on port ${API_PORT}`))
